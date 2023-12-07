58b30ac06222f49045795e9b051b4793ba646372
Shaheer Ul Islam <54592666+CrazyGG3r@users.noreply.github.com>
2023-12-08 01:59:43 +0500

    Update convex.c
    fixed convex serial

--- convex/convex.c
+++ convex/convex.c
@@ -1,108 +1,95 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 #include <time.h>
 
 typedef struct {
-    int x, y;
+    int x;
+    int y;
 } Point;
 
+double calculate_distance(Point p1, Point p2) {
+    double dx = p1.x - p2.x;
+    double dy = p1.y - p2.y;
+    return sqrt(dx * dx + dy * dy);
+}
+
+Point* generate_points(int n) {
+    Point* points = (Point*)malloc(n * sizeof(Point));
+
+    for (int i = 0; i < n; ++i) {
+        points[i].x = rand() % n + 1;
+        points[i].y = rand() % n + 1;
+    }
+
+    return points;
+}
+
+// Check if three points make a clockwise turn
 int orientation(Point p, Point q, Point r) {
     int val = (q.y - p.y) * (r.x - q.x) - (q.x - p.x) * (r.y - q.y);
     if (val == 0) return 0; // colinear
     return (val > 0) ? 1 : 2; // clock or counterclockwise
 }
 
-void convexHull(Point points[], int n, FILE *outputFile) {
-    if (n < 3) return;
+// Check if point q is on the left side of the line formed by p and r
+int on_left_side(Point p, Point q, Point r) {
+    return orientation(p, q, r) == 2;
+}
 
-    int hullSize = 0;
-    Point hull[n];
-
-    int leftmost = 0;
-    for (int i = 1; i < n; i++)
-        if (points[i].x < points[leftmost].x)
-            leftmost = i;
-
-    int p = leftmost, q;
-    do {
-        hull[hullSize++] = points[p];
-
-        q = (p + 1) % n;
-        for (int i = 0; i < n; i++) {
-            if (orientation(points[p], points[i], points[q]) == 2)
-                q = i;
-        }
-
-        p = q;
-    } while (p != leftmost);
-
-    printf("Convex Hull Points:\n");
-    // Write convex hull points to the output file
-    fprintf(outputFile, "Convex Hull:\n");
-    for (int i = 0; i < hullSize; i++) {
-        fprintf(outputFile, "(%d, %d)\n", hull[i].x, hull[i].y);
-        printf("(%d, %d)\n", hull[i].x, hull[i].y);
+int count_convex_hull_points(Point* points, int n) {
+    if (n < 3) {
+        printf("Convex hull not possible with less than 3 points.\n");
+        return 0;
     }
+
+    int convexHullCount = 0;
+
+    for (int i = 0; i < n - 2; ++i) {
+        for (int j = i + 1; j < n - 1; ++j) {
+            for (int k = j + 1; k < n; ++k) {
+                int is_convex = 1;
+
+                for (int m = 0; m < n; ++m) {
+                    if (m != i && m != j && m != k) {
+                        if (on_left_side(points[i], points[j], points[k]) !=
+                            on_left_side(points[i], points[j], points[m])) {
+                            is_convex = 0;
+                            break;
+                        }
+                    }
+                }
+
+                if (is_convex) {
+                    convexHullCount++;
+                }
+            }
+        }
+    }
+
+    return convexHullCount;
 }
 
 int main() {
-    FILE *inputFile, *outputFile;
-    inputFile = fopen("points.txt", "w");
-    if (inputFile == NULL) {
-        perror("Error opening file");
-        return 1;
-    }
+    srand(time(NULL));
 
-    int numPoints;
+    int n;
+    printf("Enter the number of points (n): ");
+    scanf("%d", &n);
 
-    printf("Enter the number of points: ");
-    scanf("%d", &numPoints);
+    Point* generated_points = generate_points(n);
 
-    // Generate random points and write to the "points.txt" file
-    fprintf(inputFile, "%d\n", numPoints);
-    for (int i = 0; i < numPoints; i++) {
-        Point p;
-        p.x = rand() % 100;
-        p.y = rand() % 100;
-        fprintf(inputFile, "%d %d\n", p.x, p.y);
-    }
+    clock_t start_time = clock();
 
-    fclose(inputFile);
+    int convexHullCount = count_convex_hull_points(generated_points, n);
 
-    // Open "points.txt" for reading
-    inputFile = fopen("points.txt", "r");
-    if (inputFile == NULL) {
-        perror("Error opening file");
-        return 1;
-    }
+    clock_t end_time = clock();
+    double cpu_time_used = ((double) (end_time - start_time)) / CLOCKS_PER_SEC;
 
-    // Read points from "points.txt"
-    fscanf(inputFile, "%d", &numPoints);
-    Point points[numPoints];
-    for (int i = 0; i < numPoints; i++)
-        fscanf(inputFile, "%d %d", &points[i].x, &points[i].y);
+    printf("Number of points in the convex hull: %d\n", convexHullCount);
+    printf("Time taken to calculate convex hull: %.6f seconds\n", cpu_time_used);
 
-    fclose(inputFile);
-
-
-
-    // Open "convex_hull.txt" for writing
-    outputFile = fopen("convex_hull.txt", "w");
-    if (outputFile == NULL) {
-        perror("Error creating file");
-        return 1;
-    }
-
-    // Measure time for convex hull calculation
-    clock_t start = clock();
-    convexHull(points, numPoints, outputFile);
-    clock_t end = clock();
-
-    // Calculate the time taken
-    double timeTaken = ((double)(end - start)) / CLOCKS_PER_SEC;
-    printf("Time taken to calculate convex hull: %f seconds\n", timeTaken);
-
-    fclose(outputFile);
+    free(generated_points);
 
     return 0;
 }
